efcc3cd6e1753f82549c4815bafb53b790c133ff
Star ⭐ <91134654+cstarflowers@users.noreply.github.com>
2026-09-13 21:33:56 -0400

    Revert changes

--- movies/views.py
+++ movies/views.py
@@ -59,8 +59,3 @@ def delete_review(request, id, review_id):
         user=request.user)
     review.delete()
     return redirect('movies.show', id=id)
-@login_required
-def report_review(request, id, review_id):
-    review = get_object_or_404(Review, id=review_id,
-        user=request.user)
-    return redirect('movies.show', id=id)
