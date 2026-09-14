c93f9aedcaf74d742347babc09bf7627c6e313b9
Star ⭐ <91134654+cstarflowers@users.noreply.github.com>
2026-09-13 21:25:21 -0400

    Fix server crash

--- movies/models.py
+++ movies/models.py
@@ -17,6 +17,6 @@ class Review(models.Model):
         on_delete=models.CASCADE)
     user = models.ForeignKey(User,
         on_delete=models.CASCADE)
-    isReported = models.BooleanField(False)
+    isReported = models.BooleanField(default=False)
     def __str__(self):
         return str(self.id) + ' - ' + self.movie.name
