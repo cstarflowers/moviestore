132499642c0543995a717e42b195669076d9a50c
Star ⭐ <91134654+cstarflowers@users.noreply.github.com>
2026-09-13 21:40:20 -0400

    Revert "Revert changes"
    This reverts commit efcc3cd6e1753f82549c4815bafb53b790c133ff.

--- movies/views.py
+++ movies/views.py
@@ -59,3 +59,8 @@ def delete_review(request, id, review_id):
         user=request.user)
     review.delete()
     return redirect('movies.show', id=id)
+@login_required
+def report_review(request, id, review_id):
+    review = get_object_or_404(Review, id=review_id,
+        user=request.user)
+    return redirect('movies.show', id=id)
