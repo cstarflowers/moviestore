a5b5ea55301afb05cd471a9a82a5b192c68f92aa
Star ⭐ <91134654+cstarflowers@users.noreply.github.com>
2026-09-13 22:45:31 -0400

    Update views.py

--- movies/views.py
+++ movies/views.py
@@ -63,5 +63,4 @@ def delete_review(request, id, review_id):
 def report_review(request, id, review_id):
     review = get_object_or_404(Review, id=review_id,
         user=request.user)
-    review.delete()
     return redirect('movies.show', id=id)
