f7ddd9297e48942b03ef7037aec4c693f7d0dce0
Star ⭐ <91134654+cstarflowers@users.noreply.github.com>
2026-09-13 21:40:38 -0400

    Revert "Attempt add reports"
    This reverts commit d96a7c9b95597685a7377d49e693a3bffbf9177d.

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from django.db import models
 from django.contrib.auth.models import User
 class Movie(models.Model):
@@ -17,6 +18,5 @@ class Review(models.Model):
         on_delete=models.CASCADE)
     user = models.ForeignKey(User,
         on_delete=models.CASCADE)
-    isReported = models.BooleanField(False)
     def __str__(self):
         return str(self.id) + ' - ' + self.movie.name
--- movies/views.py
+++ movies/views.py
@@ -59,8 +59,3 @@ def delete_review(request, id, review_id):
         user=request.user)
     review.delete()
     return redirect('movies.show', id=id)
-def report_review(request, id, review_id):
-    review = get_object_or_404(Review, id=review_id,
-        user=request.user)
-    review.isReported = True
-    return redirect('movies.show', id=id)
